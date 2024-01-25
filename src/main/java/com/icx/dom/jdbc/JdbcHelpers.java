3713f522db26460e2d247d93ea7c1cef26cf7945
Rainer Baumgärtel <rainer.baumgaertel@infinit.cx>
2024-01-25 10:56:18 +0100

    import LocalDate

--- src/main/java/com/icx/dom/jdbc/JdbcHelpers.java
+++ src/main/java/com/icx/dom/jdbc/JdbcHelpers.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
