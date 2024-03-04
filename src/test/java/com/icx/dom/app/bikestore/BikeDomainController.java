83600b136c2a98411e9460610422b5b1fd02cdec
Rainer Baumgärtel <rainer.baumgaertel@infinit.cx>
2024-03-04 14:05:19 +0100

    Comment

--- src/test/java/com/icx/dom/app/bikestore/BikeDomainController.java
+++ src/test/java/com/icx/dom/app/bikestore/BikeDomainController.java
@@ -5,6 +5,7 @@ import com.icx.domain.sql.SqlDomainObject;
 
 public class BikeDomainController extends SqlDomainController {
 
+	// Method may be overridden by a more sophisticated one
 	@Override
 	protected synchronized <S extends SqlDomainObject> long generateUniqueId(Class<S> domainObjectClass) {
 		return super.generateUniqueId(domainObjectClass);
