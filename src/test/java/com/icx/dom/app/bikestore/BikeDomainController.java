e681f810632bd262b1683b4fea3ec88d24ed0a7f
Rainer Baumgärtel <rainer.baumgaertel@infinit.cx>
2024-03-04 16:23:41 +0100

    Adaption for new generateUniqueId()

--- src/test/java/com/icx/dom/app/bikestore/BikeDomainController.java
+++ src/test/java/com/icx/dom/app/bikestore/BikeDomainController.java
@@ -1,14 +1,13 @@
 package com.icx.dom.app.bikestore;
 
 import com.icx.domain.sql.SqlDomainController;
-import com.icx.domain.sql.SqlDomainObject;
 
 public class BikeDomainController extends SqlDomainController {
 
 	// Method may be overridden by a more sophisticated one
 	@Override
-	protected synchronized <S extends SqlDomainObject> long generateUniqueId(Class<S> domainObjectClass) {
-		return super.generateUniqueId(domainObjectClass);
+	protected long generateUniqueId() {
+		return super.generateUniqueId();
 	}
 
 }
