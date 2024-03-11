7cbce222ca590e9c3590c4644ff630afc42ba5cd
Rainer Baumgärtel <rainer.baumgaertel@infinit.cx>
2024-03-11 18:02:44 +0100

    V1.0.1

--- sonarcube.bat
+++
@@ -1,1 +0,0 @@
-./gradlew --info --stacktrace sonarqube -x test -Dsonar.projectKey=cx.infinit.Domain -Dsonar.host.url=https://sonarqube.int.infinit.cx -Dsonar.login=094cf553a2b7d64c6ed0fd3f14222b2bb026173d -Dsonar.scm.disabled=true
